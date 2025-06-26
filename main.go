3e87ac577c089c20266308560c6f3edb0ecd2dc4
Lukas Peter Aldershaab <lukas.aldershaab@lego.com>
2025-06-26 13:31:48 +0200

    Increase max length of descriptions
    Signed-off-by: Lukas Peter Aldershaab <lukas.aldershaab@lego.com>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/LEGO/kube-tf-reconciler/cmd"
 
 // Generate CRDs
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.17.2 rbac:roleName=manager-role crd:maxDescLen=256 object paths="./api/..." output:crd:dir=crds
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.17.2 rbac:roleName=manager-role crd:maxDescLen=1024 object paths="./api/..." output:crd:dir=crds
 // Prepare testing executables
 //go:generate go run sigs.k8s.io/controller-runtime/tools/setup-envtest@release-0.20 use 1.32.0 --bin-dir bin/ -p path
 
