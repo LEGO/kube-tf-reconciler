c2f7368a1e1f5325f26955defaba8be367ab437a
Lukas Peter Aldershaab <lukas.aldershaab@lego.com>
2025-10-23 14:54:46 +0200

    Remove unused operator

--- cmd/config.go
+++ cmd/config.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
+	"github.com/LEGO/kube-tf-reconciler/internal/controller"
 	"github.com/LEGO/kube-tf-reconciler/pkg/fang"
-	"github.com/LEGO/kube-tf-reconciler/pkg/operator"
 )
 
-func ConfigFromEnvironment() (operator.Config, error) {
+func ConfigFromEnvironment() (controller.Config, error) {
 	return ConfigFromEnvironmentWithPrefix("KREC")
 }
 
-func ConfigFromEnvironmentWithPrefix(envPrefix string) (operator.Config, error) {
-	loader := fang.New[operator.Config]().
-		WithDefault(operator.DefaultConfig()).
+func ConfigFromEnvironmentWithPrefix(envPrefix string) (controller.Config, error) {
+	loader := fang.New[controller.Config]().
+		WithDefault(controller.DefaultConfig()).
 		WithAutomaticEnv(envPrefix).
 		WithConfigFile(fang.ConfigFileOptions{
 			Paths: []string{"$HOME", "."},
