8631014811a8e7b7eb78e9051e9ceed4e3473db0
Lorenzo Setale <lorenzo@setale.me>
2026-06-09 09:58:02 +0200

    feat: add nix and nixos support (#152)
    Adds nix and nixos support for NixOS, nix-darwin, home-manager.
    
    ---------
    
    Signed-off-by: Lorenzo Setale <lorenzo.setale@LEGO.com>

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Kubernetes operator for managing infrastructure as code using Terraform";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      krec = pkgs.buildGoModule {
+        pname = "krec";
+        version = "nix";
+        src = ./.;
+        subPackages = ["."];
+
+        # How to get the vendorHash:
+        # Option 1: set to lib.fakeHash and paste the value at build time
+        # Option 2: go mod vendor && nix hash path --sri ./vendor
+        vendorHash = "sha256-7oXuw88kHgTvs2MEu5tmNMUK1DbbK0YyYK62EBjAaIw=";
+
+        env.CGO_ENABLED = "0";
+
+        ldflags = [
+          "-s"
+          "-w"
+          # NOTE: Using tag would make the nix build impure. Using git commit and date
+          "-X github.com/LEGO/kube-tf-reconciler/cmd.version=nix"
+          "-X github.com/LEGO/kube-tf-reconciler/cmd.commit=${self.shortRev or "dirty"}"
+          "-X github.com/LEGO/kube-tf-reconciler/cmd.date=${self.lastModifiedDate or "19700101000000"}"
+        ];
+
+        postInstall = ''
+          mv $out/bin/kube-tf-reconciler $out/bin/krec
+        '';
+
+        meta = {
+          description = "Kubernetes operator for managing infrastructure as code using Terraform";
+          homepage = "https://github.com/LEGO/kube-tf-reconciler";
+          license = pkgs.lib.licenses.asl20;
+          mainProgram = "krec";
+        };
+      };
+    in {
+      inherit krec;
+      default = krec;
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          kubectl
+          kind
+        ];
+      };
+    });
+  };
+}
